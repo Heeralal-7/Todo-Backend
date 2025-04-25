88dbc7c2c857b42dd394293853400970f5066f61
Heeralal-7 <heera.hlp77@gmail.com>
2025-04-25 23:00:16 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 //models/db.js
 const mongoose = require("mongoose");
-const mongo_url = process.env.MONGO_URI;
+const mongo_url = process.env.MONGO_URI || "mongodb+srv://heerahlp77:x8MaJxm0cmPrOOde@signupdb.ro44sin.mongodb.net/";
 
 mongoose.connect(mongo_url)
 .then(()=>{
